15e6293c22c4c803b424b2f00c9fad819d06e7da
kunj <vibeleo123@gmail.com>
2025-01-11 15:29:53 +0530

    changed design of main page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-    <title>Second Brain</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <title>Goal Tracker</title>
     {{-- for css from different blade files --}}
     <style>
         .main_wrapper{
@@ -42,10 +43,8 @@
         <div class="view_container">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Second Brain</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                            aria-label="Toggle navigation">
+                        <a class="navbar-brand" href="#">Goal Tracker</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -80,7 +79,7 @@
             </div>
         </div>
     {{-- nav ends ```--}}
-    <main class="main_wrapper row mt-2">
+    <main class="main_wrapper mt-2">
         {{ $slot }}
     </main>
 </body>
