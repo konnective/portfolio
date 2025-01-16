0e2b0867efc58362548f331c78193af166b1e667
kunj <vibeleo123@gmail.com>
2025-01-16 22:04:48 +0530

    added a logic for adding marker into popup

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js">
+    </script>
     <title>Second Brain</title>
     {{-- for css from different blade files --}}
     <style>
@@ -50,7 +52,7 @@
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
+                                {{-- <li class="nav-item">
                                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                                 </li>
                                 <li class="nav-item">
@@ -72,7 +74,7 @@
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                                </li>
+                                </li> --}}
                             </ul>
                         </div>
                     </div>
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -39,102 +39,99 @@
             flex: 1;
             padding: 20px;
         }
-        .marker_wrapper{
+
+        .marker_wrapper {
             display: flex;
             flex-wrap: wrap;
             gap: 2px;
         }
-        .marker{
-            padding: 10px;
-            border-radius: 5px;
-            background-color: #198754;
-        }
-        .marked{
+
+        .marker {
             padding: 10px;
             border-radius: 5px;
             background-color: #80edba;
         }
+
+        .marked {
+            padding: 10px;
+            border-radius: 5px;
+            background-color: #198754;
+        }
     </style>
 @endpush
 <x-layout>
-    <div class="card_wrapper">
-        @foreach ($projects as $item)
-            <div class="product-card">
-                <div class="row">
-                    <div class="col-6">
-                        <a href="{{ route('note', $item->id) }}">
-                            <div class="img-container">
-                                <img src="{{ asset('images/home.png') }}" alt="no img" />
+    
+    <div class="row">
+        <div class="row">
+            <div class="col-12 mx-4">
+                <button type="button" class="btn btn-primary float-end">
+                    Add Goal
+                </button>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            @foreach ($projects as $item)
+                <div class="product-card">
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="{{ route('note', $item->id) }}">
+                                <div class="img-container">
+                                    <img src="{{ asset('images/home.png') }}" alt="no img" />
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <div class="info-container">
+                                <span class="item-title">{{ $item->name }}</span>
+                                <span class="item-price">
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal">
+                                        Progress
+                                    </button>
+                                </span>
                             </div>
-                        </a>
-                    </div>
-                    <div class="col-6">
-                        <div class="info-container">
-                            <span class="item-title">{{ $item->name }}</span>
-                            <span class="item-price">
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                    data-bs-target="#exampleModal">
-                                    Progress
-                                </button>
-                            </span>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
+        </div>
 
-            {{-- code for modal here --}}
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Details</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="marker_wrapper">
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marker"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                                <div class="marked"></div>
-                            </div>
+        {{-- code for modal here --}}
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Details</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="marker_wrapper">
+
                         </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        </div>
     </div>
 </x-layout>
 <script>
-    $(".btn-primary").on("click",function(){
-    var url = "{{route('project.data',1)}}";
-    $.ajax({
-       type:"GET",
-       url:"",
-       beforeSend:function(){
-            console.log("waiting ....")
-       },
-       success:function(data){
-        console.log(data)
-      }
-   });
-});
+    $(".btn-primary").on("click", function() {
+        var url = "{{ route('project.data', 1) }}";
+        $.ajax({
+            type: "GET",
+            url: url,
+            beforeSend: function() {
+                console.log("waiting ....")
+                const loader = '<p>Loading ...</p>'
+                $('.marker_wrapper').empty();
+                $('.marker_wrapper').append(loader);
+            },
+            success: function(data) {
+                console.log(data)
+                $('.marker_wrapper').empty();
+                $('.marker_wrapper').append(data.html);
+
+            }
+        });
+    });
 </script>
