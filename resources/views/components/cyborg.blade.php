a46ea38703b3034b6316eb4d10e660a2218718fe
kunj <vibeleo123@gmail.com>
2025-02-06 23:23:08 +0530

    added a register button to header

--- resources/views/components/cyborg.blade.php
+++ resources/views/components/cyborg.blade.php
@@ -32,44 +32,7 @@
 
 <body>
     <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="" class="logo">
-                            <span class="logo_main">Second-Brain</span>
-                        </a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Search End ***** -->
-                        <div class="search-input">
-                            <form id="search" action="#">
-                                <input type="text" placeholder="Type Something" id='searchText' name="searchKeyword"
-                                    onkeypress="handle" />
-                                <i class="fa fa-search"></i>
-                            </form>
-                        </div>
-                        <!-- ***** Search End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li><a href=""class="active">Home</a></li>
-                            <li><a href="">Browse</a></li>
-                            <li><a href="">Details</a></li>
-                            <li><a href="">Streams</a></li>
-                            <li><a href="">Profile <img
-                                        src="{{ asset('cyborg/assets/images/profile-header.jpg') }}" alt=""></a>
-                            </li>
-                        </ul>
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
+    
     <!-- ***** Header Area End ***** -->
 
     <!-- ***** Preloader Start ***** -->
@@ -107,10 +70,8 @@
                         <!-- ***** Search End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
-                            <li><a href=""class="active">Home</a></li>
-                            <li><a href="">Browse</a></li>
-                            <li><a href="">Details</a></li>
-                            <li><a href="">Streams</a></li>
+                            <li><a href="{{route('register')}}" class="active">Register</a></li>
+                           
                             <li><a href="">Profile <img
                                         src="{{ asset('cyborg/assets/images/profile-header.jpg') }}" alt=""></a>
                             </li>
