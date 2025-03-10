5e7fce3faf7c2459fdb817a79a841bc58d24b908
kunj <vibeleo123@gmail.com>
2025-03-10 17:47:24 +0530

    added a url or local host of win for faster work

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+
+    //http://localhost/live_project/portfolio/
     //fields to add to table name,date,user_id,status,project_id
     // ALTER TABLE `tasks` ADD `end_date` DATE NULL DEFAULT NULL AFTER `name`;
     // ALTER TABLE `users` ADD `partner` BOOLEAN NOT NULL DEFAULT FALSE AFTER `remember_token`;
