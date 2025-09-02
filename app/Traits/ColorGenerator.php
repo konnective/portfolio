2b001f6251ef8e02140b6f1e5bfe2db28887cb48
kunj <vibeleo123@gmail.com>
2025-09-02 22:14:24 +0530

    code inprovement #1

--- app/Traits/ColorGenerator.php
+++ app/Traits/ColorGenerator.php
@@ -10,7 +10,6 @@ trait ColorGenerator
         $red = mt_rand(0, 255);
         $green = mt_rand(0, 255);
         $blue = mt_rand(0, 255);
-
         $hexRed = sprintf("%02x", $red);
         $hexGreen = sprintf("%02x", $green);
         $hexBlue = sprintf("%02x", $blue);
