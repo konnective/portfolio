984eae1326d999354c2a3e0a45155809af59251e
kunj <vibeleo123@gmail.com>
2025-10-31 18:10:38 +0530

    created controller  for blog for api development

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\api\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -6,3 +8,7 @@ Route::get('/homeless', function () {
     $serverName = $_SERVER['SERVER_NAME'];
     return $serverName;
 });
+Route::get("blogs", [BlogController::class, "blogs"])->name("blogs");
+
+
+
